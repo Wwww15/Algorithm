667fd63512ae0c22c65b5f34bf41420cc5cfe838
文志勋 <1006380802@qq.com>
2022-04-12 12:13:29 +0800

    1.数组转稀疏数组 2.数组模拟队列

---
+++ DataStructures/src/main/java/sparsearray/Queue.java
@@ -0,0 +1,122 @@
+package sparsearray;
+
+import java.util.Scanner;
+
+/**
+ * 银行排队（队列）
+ * 特点：
+ *  1.先进先出
+ *  2.可以用数组或者是链表实现
+ *  3.队尾加数据
+ *  4.队首取数据
+ */
+public class Queue {
+
+    public static void main(String[] args) {
+        ArrayQueue arrayQueue = new ArrayQueue(3);
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        while(loop) {
+            switch (scanner.next()) {
+                case "A":
+                    break;
+            }
+        }
+    }
+}
+
+/**
+ * 数组模拟队列（不可复用）
+ */
+class  ArrayQueue {
+
+    /**
+     * 队列
+     */
+    private int[] arr;
+    /**
+     * 队首
+     */
+    private int front;
+    /**
+     * 队尾
+     */
+    private int rear;
+    /**
+     * 最大容量
+     */
+    private int maxSize;
+
+    public ArrayQueue(int arrMaxSize) {
+        this.arr = new int[arrMaxSize];
+        this.maxSize = arrMaxSize;
+    }
+
+    /**
+     * 是否满
+     * @return
+     */
+    public boolean isFull() {
+        return rear == this.maxSize-1;
+    }
+
+    /**
+     * 添加
+     */
+    public void addQueue(int number) {
+        if(isFull()) {
+            System.out.println("当前队列已满！");
+        }
+        arr[rear] = number;
+        rear++;
+    }
+
+    /**
+     * 是否空
+     */
+    public boolean isEmpty() {
+        return rear == front;
+    }
+
+    /**
+     * 获取数据
+     */
+    public int getQueue() {
+        if(isEmpty()) {
+            throw new RuntimeException("当前队列为空！");
+        }
+        int result = arr[front];
+        front++;
+        return result;
+    }
+
+    /**
+     * 获取队列首部
+     */
+    public int getHeader() {
+        if(isEmpty()) {
+            throw new RuntimeException("当前队列为空！");
+        }
+        int result = arr[front];
+        return result;
+    }
+
+    /**
+     * 展示queue
+     */
+    public void showQueue() {
+        if(isEmpty()) {
+            throw new RuntimeException("当前队列为空！");
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("arr[%d]=%d",i,arr[i]);
+        }
+    }
+
+}
+//数组模拟队列
+//1.队首
+//2.队尾
+//3.数组长度
+//4.取数据
+//5.放数据
--- DataStructures/src/main/java/sparsearray/SparseArray.java
+++ DataStructures/src/main/java/sparsearray/SparseArray.java
@@ -1,5 +1,11 @@
 package sparsearray;
 
+/**
+ * 围棋存盘，稀疏数组
+ * 特点；
+ *  1.第一行表示几行几列，有多少个不同的值
+ *  2.缩小数据
+ */
 public class SparseArray {
 
     public static void main(String[] args) {
@@ -7,6 +13,72 @@ public class SparseArray {
         //0：表示没有棋子
         //1：表示黑子
         //2：表示蓝子
-        int chessArr[][] = new
+        int chessArr[][] = new int[11][11];
+        //放入棋子
+        chessArr[1][2] = 1;
+        chessArr[2][3] = 2;
+        chessArr[3][4] = 2;
+        //查看棋盘
+        System.out.println("当前棋盘：");
+        for (int i[] : chessArr) {
+            for (int j = 0; j < i.length; j++) {
+                System.out.print(i[j]+"\t");
+            }
+            System.out.println("\n");
+        }
+
+        //存盘
+        //将二维数据转化为稀疏数组
+        //1.获取稀疏数组的大小
+        int sum = 0;
+        for (int i[] : chessArr) {
+            for (int j = 0; j < i.length; j++) {
+               if(i[j] !=0 ) {
+                   sum++;
+               }
+            }
+        }
+        //2.创建稀疏数组
+        int sparseArr[][] = new int[sum+1][3];
+        sparseArr[0][0] = chessArr.length;
+        sparseArr[0][1] = chessArr[0].length;
+        sparseArr[0][2] = sum;
+        int row = 1;
+        //3.存值
+        for (int i=0;i<chessArr.length;i++) {
+            for (int j = 0; j < chessArr[i].length; j++) {
+                if(chessArr[i][j] !=0 ) {
+                    sparseArr[row][0] = i;
+                    sparseArr[row][1] = j;
+                    sparseArr[row][2] = chessArr[i][j];
+                    row++;
+                }
+            }
+        }
+        //4.查看压缩后的稀疏数组
+        System.out.println("存盘后：");
+        for (int i[]:sparseArr) {
+            for (int j = 0; j <  i.length; j++) {
+                System.out.print(i[j]+"\t");
+            }
+            System.out.println("\n");
+        }
+
+        //复盘
+        //将稀疏数组恢复成二维数组
+        //1.先获取稀疏数组第一行数据，生成二维数组
+        int chessBackArr[][] = new int[sparseArr[0][0]][sparseArr[0][1]];
+        //2.遍历稀疏数组赋值给二维数组
+        for (int i = 1; i < sparseArr.length; i++) {
+            chessBackArr[sparseArr[i][0]][sparseArr[i][1]] = sparseArr[i][2];
+        }
+        //3.打印生成后的二维数据
+        System.out.println("复盘后的二维数组：");
+        for (int i = 0; i < chessBackArr.length; i++) {
+            for (int j = 0; j < chessBackArr[i].length; j++) {
+                System.out.print(chessBackArr[i][j]+"\t");
+            }
+            System.out.println("\n");
+        }
     }
 }
