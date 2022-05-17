204d1bcf7e42db1473a98bb1d74cfaac8f4b6383
Wenzx <53310640+Wenzx1993@users.noreply.github.com>
2022-05-17 22:40:28 +0800

    1.顺序查找（线性查找）

---
+++ DataStructures/src/main/java/search/BinarySearch.java
@@ -0,0 +1,8 @@
+package search;
+
+/**
+ * 查询算法：二分查找算法（折半算法）
+ * 注意：二分查找算法需要当前的数组是有序的
+ */
+public class BinarySearch {
+}
---
+++ DataStructures/src/main/java/search/SeqSearch.java
@@ -0,0 +1,7 @@
+package search;
+
+/**
+ * 查找算法：顺序查找（线性查找）
+ */
+public class SeqSearch {
+}
