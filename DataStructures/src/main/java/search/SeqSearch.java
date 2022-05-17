dd74f69b6ab819a3d9e9befdd2d06f3656e4c591
Wenzx <53310640+Wenzx1993@users.noreply.github.com>
2022-05-17 23:29:32 +0800

    Revert "1.顺序查找（线性查找） 2.二分查找 3.插值查找"
    This reverts commit c25a67db

--- DataStructures/src/main/java/search/SeqSearch.java
+++ DataStructures/src/main/java/search/SeqSearch.java
@@ -4,19 +4,4 @@ package search;
  * 查找算法：顺序查找（线性查找）
  */
 public class SeqSearch {
-
-    public static void main(String[] args) {
-        int arr[] = {1,2,3,4,599,999,123,-1,-90};
-        int searchIndex = seqSearch(arr, 99);
-        System.out.println("当前找到的下标为："+searchIndex);
-    }
-
-    public static int seqSearch(int[] arr,int findVal) {
-        for (int i = 0; i <arr.length ; i++) {
-            if(arr[i]==findVal) {
-                return i;
-            }
-        }
-        return -1;
-    }
 }
